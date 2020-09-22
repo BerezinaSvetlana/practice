0946ac7e4247046e364876e2df2f837c65f1ac2f
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-22 20:43:51 +0400

    Cоздала поле под игровую ячейку.

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -1,12 +1,14 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.Snake;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameField extends JPanel {
 
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
+    private Image dot;
 
     public GameField(){
 
