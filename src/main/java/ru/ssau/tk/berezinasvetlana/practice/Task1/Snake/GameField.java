b4696054ba9ec3744022bcf7a0d14b3c1c624053
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-22 20:45:06 +0400

    Cоздала поле под игровую ячейку - яблочко.

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -9,6 +9,8 @@ public class GameField extends JPanel {
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
     private Image dot;
+    private Image apple;
+
 
     public GameField(){
 
