cbf1e63fb60483e253cad77f33779aa29a3b6db8
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-22 20:18:43 +0400

    Добавила поле с размером поля

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -3,4 +3,10 @@ package ru.ssau.tk.berezinasvetlana.practice.Task1.Snake;
 import javax.swing.*;
 
 public class GameField extends JPanel {
+
+    private final int SIZE = 320;
+
+    public GameField(){
+
+    }
 }
