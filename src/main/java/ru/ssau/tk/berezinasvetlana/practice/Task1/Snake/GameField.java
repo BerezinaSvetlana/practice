e5affa591f9eafa2efa1c5950a4cc1f31b308c33
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-23 17:58:48 +0400

    расписали все граничные условия

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -102,6 +102,15 @@ public class GameField extends JPanel implements ActionListener {
         if (x[0] > SIZE){
             inGame = false;
         }
+        if (x[0] < 0){
+            inGame = false;
+        }
+        if (y[0] > SIZE){
+            inGame = false;
+        }
+        if (y[0] < 0){
+            inGame = false;
+        }
     }
 
     @Override
