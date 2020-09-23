117003a82b9675b076bc1a2bb0928c09431e1d47
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-23 18:49:40 +0400

    Создала поля класса Point и конструктор

---
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/Point.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.berezinasvetlana.practice.Task1.Point;
+
+public class Point {
+    private final double x;
+    private final double y;
+    private final double z;
+
+    Point(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
