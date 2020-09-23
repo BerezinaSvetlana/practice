41834b938c0c91ee13941402d49478142ca20a81
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-23 17:55:45 +0400

    прописала, что при столкновении с собой, мы проигрываем

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -94,6 +94,7 @@ public class GameField extends JPanel implements ActionListener {
     public void checkCollisions(){
         for (int i = dots; i > 0; i--) {
             if(i>4 && x[0] == x[i] && y[0] == y[i]){
+                inGame = false;
 
             }
 
