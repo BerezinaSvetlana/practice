2b4e1e761575c85946cb563685039472d06e84e9
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-25 22:54:10 +0400

    бла бла бла

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/Points.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/Points.java
@@ -1,4 +1,19 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.Point;
 
-public class Points {
-}
+public  class Points extends Point {
+    double result;
+
+    Points(double x, double y, double z, double res){
+        super(x, y, z);
+        result = res;
+    }
+
+    public static double sum(double a, double b, double c){
+
+        return a+b;
+
+
+    }
+
+
+
