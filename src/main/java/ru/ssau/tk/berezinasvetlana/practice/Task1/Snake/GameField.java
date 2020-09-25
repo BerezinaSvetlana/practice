7ef38102b51bdf571eb0e6463707c0d42b5e587e
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-25 19:57:18 +0400

    Сделала так, чтобы надпись была голубой

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Snake/GameField.java
@@ -70,6 +70,7 @@ public class GameField extends JPanel implements ActionListener {
         else {
             String str = "Game over";
             Font f = new Font("Arial", 14, Font.BOLD);
+            g.setColor(Color.blue);
 
         }
     }
