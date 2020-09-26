bc74db11e1d6e6e815fc5b1bf9d4e30823af0ee2
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 22:26:06 +0400

    Расчертили игровое поле. Проще через цикл фор, но можно было и в ручную это прописать было

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -13,6 +13,14 @@ public class BattleShip {
         playerName1 = scanner.nextLine();
         System.out.println("Player#2, please enter your name: ");
         playerName2 = scanner.nextLine();
+
+        System.out.println();
+        System.out.println(playerName1 + "please place your ship on the battlefield");
+        System.out.println();
+        System.out.println("  0 1 2 3 4 5 6 7 8 9");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " - - - - - - - - - -");
+        }
     }
 
     public static void draw() {
