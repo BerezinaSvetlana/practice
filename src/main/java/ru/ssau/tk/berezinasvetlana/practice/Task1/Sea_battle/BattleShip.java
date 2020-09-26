d27341d1938f0e3d59c32c20063977922f2323a6
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 22:04:22 +0400

    Хочу чтобы имена вводились с консоли, для этого прописываю сканер

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -1,9 +1,12 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.Sea_battle;
 
+import java.util.Scanner;
+
 public class BattleShip {
 
     static String playerName1;
     static String playerName2;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Player#1, please enter your name: ");
