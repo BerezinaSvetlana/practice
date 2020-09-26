b5c286ef0f7933bf5d2dc8bfb4034f932a956d78
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 21:51:34 +0400

    Переименовала переменные, чтобы было лучше полнять, что в ним мы будем хванить данные об именах игроков

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -2,8 +2,8 @@ package ru.ssau.tk.berezinasvetlana.practice.Task1.Sea_battle;
 
 public class BattleShip {
 
-    static String player1;
-    static String player2;
+    static String playerName1;
+    static String playerName2;
     public static void main(String[] args) {
 
 
