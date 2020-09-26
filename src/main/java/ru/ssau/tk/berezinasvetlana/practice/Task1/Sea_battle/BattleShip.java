a581c10cfde0e5f6734bb2744103cb44fab2b7d9
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 22:00:20 +0400

    Спрашиваем у игроков их имена

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -4,9 +4,10 @@ public class BattleShip {
 
     static String playerName1;
     static String playerName2;
+
     public static void main(String[] args) {
-
-
+        System.out.println("Player#1, please enter your name: ");
+        System.out.println("Player#2, please enter your name: ");
     }
 
     public static void draw() {
