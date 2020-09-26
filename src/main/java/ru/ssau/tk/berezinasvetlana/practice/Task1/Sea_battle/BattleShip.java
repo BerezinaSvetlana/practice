00271391b0378121409924d1d7086ad1f7982ec9
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 21:50:14 +0400

    Добавила static

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -2,8 +2,8 @@ package ru.ssau.tk.berezinasvetlana.practice.Task1.Sea_battle;
 
 public class BattleShip {
 
-    String player1;
-    String player2;
+    static String player1;
+    static String player2;
     public static void main(String[] args) {
 
 
