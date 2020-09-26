4daaba7ed5c3a45b2dce9bc6465fedeb26e967e2
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 20:53:35 +0400

    попробовала реализовать сумму

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/Points.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/Points.java
@@ -3,17 +3,18 @@ package ru.ssau.tk.berezinasvetlana.practice.Task1.Point;
 public  class Points extends Point {
     double result;
 
-    Points(double x, double y, double z, double res){
+    Points(double x, double y, double z, double res) {
         super(x, y, z);
         result = res;
     }
 
     public static double sum(double a, double b, double c){
 
-        return a+b;
 
+     return  a+b;
 
-    }
+}
+}
 
 
 
