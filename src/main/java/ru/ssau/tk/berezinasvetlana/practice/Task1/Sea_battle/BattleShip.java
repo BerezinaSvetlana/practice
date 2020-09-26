8fb1e89e23a53cd84ccf5e4dc056d1ebce7fcb53
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 22:29:07 +0400

    Хочу занести рисовку поля в специально написанный метод, чтобы было проще рисовать поле, просто вызвав метод

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -15,7 +15,7 @@ public class BattleShip {
         playerName2 = scanner.nextLine();
 
         System.out.println();
-        System.out.println(playerName1 + "please place your ship on the battlefield");
+        System.out.println(playerName1 + " please place your ship on the battlefield");
         System.out.println();
         System.out.println("  0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < 10; i++) {
@@ -23,7 +23,7 @@ public class BattleShip {
         }
     }
 
-    public static void draw() {
+    public static void drawField() {
 
     }
 
