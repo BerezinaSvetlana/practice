6dc5b408e581b043468e5fdd3fe39b92cb2d1dee
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-26 21:45:22 +0400

    Создала переменную для второго игрока

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -3,6 +3,7 @@ package ru.ssau.tk.berezinasvetlana.practice.Task1.Sea_battle;
 public class BattleShip {
 
     String player1;
+    String player2;
     public static void main(String[] args) {
 
 
