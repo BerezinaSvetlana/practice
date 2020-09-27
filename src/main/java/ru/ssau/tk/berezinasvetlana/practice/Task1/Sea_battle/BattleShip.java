2943f4983689ca2fb71a0de0cd3444043acd3e79
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-09-27 20:06:23 +0400

    Создала двумерный массив, чтобы хранить в нем координаты корабликов

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -7,6 +7,7 @@ public class BattleShip {
     static String playerName1;
     static String playerName2;
     static Scanner scanner = new Scanner(System.in);
+    static int [][] array = new int[10][10];
 
     public static void main(String[] args) {
         System.out.println("Player#1, please enter your name: ");
