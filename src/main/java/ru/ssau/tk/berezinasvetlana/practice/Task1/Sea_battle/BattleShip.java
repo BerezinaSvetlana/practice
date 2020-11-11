2405ecab5157b016f19501e1abd3b81a1680b75c
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-11-11 16:25:19 +0400

    поняла, что нужен другой метод для рисовки поля, так как с добавлением корабликов поле не должно оставаться пустым и должно меняться. Ну и подругому назвала старый метод, так как он отвечает за расстановку корабликов на поле

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -18,16 +18,14 @@ public class BattleShip {
         System.out.println("Участики, учтановите по одному однопалубному, двухпалубному, трехпалубному и четырехпалубному кораблю.");
     }
 
-    public static void drawField() {
+    public static void placeShips() {
         int deck = 4;
         while (deck >= 1) {
             System.out.println();
             System.out.println(playerName1 + " please place your" + deck + "-deck ship on the battlefield");
             System.out.println();
             System.out.println(" 0 1 2 3 4 5 6 7 8 9");
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i + " - - - - - - - - - -");
-            }
+
             System.out.println("Please enter OX coordinate: ");
             int x = scanner.nextInt();
             System.out.println("Please enter OY coordinate: ");
@@ -38,12 +36,18 @@ public class BattleShip {
             int direction = scanner.nextInt();
             for (int i = 0; i < deck; i++) {
                 if (direction == 1) {
-                    battlefield1[x][y+i] = 1;
-                }else {
-                    battlefield1[x+i][y] = 1;
+                    battlefield1[x][y + i] = 1;
+                } else {
+                    battlefield1[x + i][y] = 1;
                 }
                 deck--;
             }
+        }
+    }
+
+    public static void drawField() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " - - - - - - - - - -");
         }
     }
 }
