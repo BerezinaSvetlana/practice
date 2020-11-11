48eef391b9fe7888c73c73976bdb7d3c9f47f03d
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-11-11 16:09:30 +0400

    запихиваю процесс расставления корабликов в цикл, попутно исправляю все недочеты

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -19,37 +19,35 @@ public class BattleShip {
     }
 
     public static void drawField() {
-        System.out.println();
-
-        System.out.println(playerName1 + " please place your 4-deck ship on the battlefield");
-        System.out.println();
-        System.out.println(" 0 1 2 3 4 5 6 7 8 9");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i + " - - - - - - - - - -");
-        }
-        System.out.println("Please enter OX coordinate: ");
-        int x = scanner.nextInt();
-        System.out.println("Please enter OY coordinate: ");
-        int y = scanner.nextInt();
-        System.out.println("Choose direction: ");
-        System.out.println("1. Vertical. ");
-        System.out.println("2. Horizontal. ");
-        int direction = scanner.nextInt();
-        battlefield1[x][y] = 1;
-        if (direction == 1) {
-            battlefield1[x][y+1] = 1;
-            battlefield1[x][y+2] = 1;
-            battlefield1[x][y+3] = 1;
-        }else {
-            battlefield1[x+1][y] = 1;
-            battlefield1[x+2][y] = 1;
-            battlefield1[x+3][y] = 1;
-        }
-
-        for (int i = 0; i < battlefield1.length; i++) {
-            for (int j = 0; j < battlefield1.length; j++) {
-
+        int deck = 4;
+        while (deck >= 1) {
+            System.out.println();
+            System.out.println(playerName1 + " please place your" + deck + "-deck ship on the battlefield");
+            System.out.println();
+            System.out.println(" 0 1 2 3 4 5 6 7 8 9");
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " - - - - - - - - - -");
             }
+            System.out.println("Please enter OX coordinate: ");
+            int x = scanner.nextInt();
+            System.out.println("Please enter OY coordinate: ");
+            int y = scanner.nextInt();
+            System.out.println("Choose direction: ");
+            System.out.println("1. Vertical. ");
+            System.out.println("2. Horizontal. ");
+            int direction = scanner.nextInt();
+            battlefield1[x][y] = 1;
+            if (direction == 1) {
+                battlefield1[x][y + 1] = 1;
+                battlefield1[x][y + 2] = 1;
+                battlefield1[x][y + 3] = 1;
+            } else {
+                battlefield1[x + 1][y] = 1;
+                battlefield1[x + 2][y] = 1;
+                battlefield1[x + 3][y] = 1;
+            }
+            deck--;
         }
     }
+}
 }
