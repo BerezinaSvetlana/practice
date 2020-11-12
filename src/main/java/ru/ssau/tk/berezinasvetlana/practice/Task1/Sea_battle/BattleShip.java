a615bb6ef16450e89e83d167925dc505b6cc549a
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-11-12 19:59:51 +0400

    ошибку не нашла, зато сделала поле соперника

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Sea_battle/BattleShip.java
@@ -9,23 +9,25 @@ public class BattleShip {
     static Scanner scanner = new Scanner(System.in);
     static int[][] battlefield1 = new int[10][10];
     static int[][] battlefield2 = new int[10][10];
+    static int[][] monitor1 = new int[10][10];
+    static int[][] monitor2 = new int[10][10];
 
     public static void main(String[] args) {
         System.out.println("Player#1, please enter your name: ");
         playerName1 = scanner.nextLine();
         System.out.println("Player#2, please enter your name: ");
         playerName2 = scanner.nextLine();
-        System.out.println("Участики, учтановите по одному однопалубному, двухпалубному, трехпалубному и четырехпалубному кораблю.");
-        placeShips();
+        placeShips(playerName1, battlefield1);
+        placeShips(playerName2, battlefield2);
     }
 
-    public static void placeShips() {
+    public static void placeShips(String playerName, int[][] battlefield) {
         int deck = 4;
         while (deck >= 1) {
             System.out.println();
-            System.out.println(playerName1 + " please place your" + deck + "-deck ship on the battlefield");
+            System.out.println(playerName + " please place your" + deck + "-deck ship on the battlefield");
             System.out.println();
-            drawField(battlefield1);
+            drawField(battlefield);
             System.out.println("Please enter OX coordinate: ");
             int x = scanner.nextInt();
             System.out.println("Please enter OY coordinate: ");
@@ -46,12 +48,31 @@ public class BattleShip {
     }
 
     public static void drawField(int[][] battlefield) {
+        System.out.println("  0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < battlefield.length; i++) {
             System.out.print(i + " ");
             for (int j = 0; j < battlefield[1].length; j++) {
 
-                if (battlefield[i][j] == 0) {
+                if (battlefield[j][i] == 0) {
                     System.out.print("- ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void makeTurn(String playerName, int[][] monitor, int[][] battlefield) {
+        System.out.println(playerName + ", please, make your turn.");
+        System.out.println("  0 1 2 3 4 5 6 7 8 9");
+        for (int i = 0; i < monitor.length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < monitor[1].length; j++) {
+                if (monitor[j][i] == 0) {
+                    System.out.print("- ");
+                } else if (monitor[j][i] == 1) {
+                    System.out.print(". ");
                 } else {
                     System.out.print("X ");
                 }
