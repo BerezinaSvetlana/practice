8378c89b25313da352084893b7bbfd405a8f7563
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-10 02:04:54 +0400

    3.3

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_3.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_3.java
@@ -1,4 +1,18 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_3 {
+    public static void main(String[] args) {
+        String str = "Hello world";
+        String str1 = new String(str);
+        String str2 = "Hello world";
+        if(str==str2){
+            System.out.println("str==str2");
+        }
+        if (str == str1) {
+            System.out.println("str==str1");
+        }
+        if (str.equals(str1)) {
+            System.out.println("str.equals(str1)");
+        }
+    }
 }
