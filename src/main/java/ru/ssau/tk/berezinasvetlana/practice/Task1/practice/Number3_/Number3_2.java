33694d39b561a4734d9b3c5b84df564a5a20ca7c
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-10 01:54:10 +0400

    3.2 and tests

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_2.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_2.java
@@ -1,4 +1,10 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_2 {
+    public void getStr(String str){
+        byte[] bytes = str.getBytes();
+        for (byte byt:bytes) {
+            System.out.println(byt);
+        }
+    }
 }
