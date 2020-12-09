36d20409727d8e2165a265fd8e62f4c3431274eb
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-10 03:03:14 +0400

    3.9 and tests

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_9.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_9.java
@@ -1,4 +1,7 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_9 {
+    static int getIndexOf(String str_1, String str_2) {
+        return str_1.lastIndexOf(str_2, str_1.length() / 2);
+    }
 }
