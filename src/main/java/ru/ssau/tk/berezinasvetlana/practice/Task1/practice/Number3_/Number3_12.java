963e501a6263086c7c3e715a2ce2467b96beedb5
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-17 19:27:15 +0400

    3.12 and tests

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_12.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_12.java
@@ -1,4 +1,10 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_12 {
+    static String Strings(String str_1, String str_2, String str_3) {
+        for (int i = 0; i < 100 && str_1.contains(str_2); i++) {
+            str_1 = str_1.replaceAll(str_2, str_3);
+        }
+        return str_1;
+    }
 }
