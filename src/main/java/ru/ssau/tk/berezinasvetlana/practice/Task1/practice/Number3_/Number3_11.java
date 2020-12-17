8564eac49201331717337b98f50f2a9a4c27cf84
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-17 18:47:29 +0400

    3.11 and tests

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_11.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_11.java
@@ -1,4 +1,16 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_11 {
+    static int NumberOfString(String[] array, String prefix, String postfix) {
+        int number = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i].trim();
+        }
+        for (String str : array) {
+            if (str.startsWith(prefix) && str.endsWith(postfix)) {
+                number++;
+            }
+        }
+        return number;
+    }
 }
