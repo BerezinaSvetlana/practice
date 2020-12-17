051625298ba1fe914b217b9e6879bf6ad86c06ab
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-17 23:57:47 +0400

    3.21

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_21.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_21.java
@@ -1,4 +1,18 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_21 {
+    public static void main(String[] args) {
+        String str = "svetlana";
+        /* 0v2t4a6a
+           a6a4t2v0
+        */
+        StringBuilder strBldr = new StringBuilder(str);
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 2 == 0) {
+                strBldr.replace(i, i + 1, i + "");
+            }
+        }
+        strBldr.reverse();
+        System.out.println(strBldr.toString());
+    }
 }
