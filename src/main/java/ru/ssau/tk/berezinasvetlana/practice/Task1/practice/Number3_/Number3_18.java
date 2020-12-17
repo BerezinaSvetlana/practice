3b6a068880b5a402ad427de0f6743aca1af5acec
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-17 23:15:34 +0400

    3,18

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_18.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_18.java
@@ -1,4 +1,8 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_18 {
+    public static void getDescription(Object obj){
+        System.out.println("Описание объекта: ");
+        System.out.println(obj);
+    }
 }
