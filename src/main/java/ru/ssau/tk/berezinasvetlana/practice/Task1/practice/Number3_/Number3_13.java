2bdec67c1ba5f6763d34eb1f2615cbb6941e97d8
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-17 19:36:40 +0400

    create 3.13 and tests

--- src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_13.java
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/practice/Number3_/Number3_13.java
@@ -1,4 +1,16 @@
 package ru.ssau.tk.berezinasvetlana.practice.Task1.practice.Number3_;
 
 public class Number3_13 {
+    static String getStringOfIndex(String str, int from, int to) {
+        if (from < 0) {
+            from = 0;
+        }
+        if (to > str.length()) {
+            to = str.length();
+        }
+        if (from == to) {
+            return "";
+        }
+        return str.substring(from, to);
+    }
 }
