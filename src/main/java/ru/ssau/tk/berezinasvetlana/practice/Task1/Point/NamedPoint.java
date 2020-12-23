3abe5d0ec507af15e693d59c66ad4418fc2ccc3a
BerezinaSvetlana <BerezinaSvetlanaRomanovna@yandex.ru>
2020-12-24 01:38:32 +0400

    NamedPoint extends Point

---
+++ src/main/java/ru/ssau/tk/berezinasvetlana/practice/Task1/Point/NamedPoint.java
@@ -0,0 +1,36 @@
+package ru.ssau.tk.berezinasvetlana.practice.Task1.Point;
+
+import java.util.Objects;
+
+public class NamedPoint extends Point{
+    public String name;
+
+    NamedPoint() {
+        this(0, 0, 0, "Origin");
+    }
+
+    public NamedPoint(double x, double y, double z) {
+        super(x, y, z);
+    }
+
+    public NamedPoint(double x, double y, double z, String name) {
+        super(x, y, z);
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        if (Objects.equals(null, name)) {
+            return super.toString();
+        }
+        return name + ": " + super.toString();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
